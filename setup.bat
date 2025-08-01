d307bd076c845d36207820dbfa5304e71fb3b822
Alesim <calejandrito317@gmail.com>
2025-08-01 01:11:29 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -15,38 +15,38 @@ IF EXIST backend\node_modules (
 echo Instalando Frontend...
 cd frontend
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ No se pudo acceder al directorio frontend
+    echo  No se pudo acceder al directorio frontend
     exit /b
 )
 
 call npm install
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ Error al instalar dependencias del frontend
+    echo  Error al instalar dependencias del frontend
     exit /b
 )
 
 call npm install react-router-dom
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ Error al instalar react-router-dom
+    echo  Error al instalar react-router-dom
     exit /b
 )
 
 echo Instalando Backend...
 cd ..\backend
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ No se pudo acceder al directorio backend
+    echo  No se pudo acceder al directorio backend
     exit /b
 )
 
 call npm install
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ Error al instalar dependencias del backend
+    echo  Error al instalar dependencias del backend
     exit /b
 )
 
 call npm install express
 IF %ERRORLEVEL% NEQ 0 (
-    echo ❌ Error al instalar express
+    echo  Error al instalar express
     exit /b
 )
 

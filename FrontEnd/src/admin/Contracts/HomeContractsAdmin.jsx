61caf520fff2d73ed05e55befa8c78cf7a18711b
AleEnMayus <calejandrito317@gmail.com>
2025-09-02 20:55:37 -0500

    Contratos finalizados

--- FrontEnd/src/admin/Contracts/HomeContractsAdmin.jsx
+++ FrontEnd/src/admin/Contracts/HomeContractsAdmin.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import HeaderAdm from "../../components/HeaderAdm";
 import '../../components/components.css';
-import './Contracts.css';
 import { useNavigate } from "react-router-dom";
 
 const ContractsAdmin = () => {
@@ -37,17 +36,14 @@ const ContractsAdmin = () => {
   };
 
   return (
-    <div className="contrato-container">
+    <div className="contrato-container mle-0">
       <HeaderAdm />
       
-      <div className="contrato-wrapper">
+      <div className="contrato-wrapper mt-10 d-flex flex-column justify-center ">
         {/* Título principal */}
-        <h1 className="contrato-title">
-          Contrato
-        </h1>
         
         {/* Sección de envío de contrato */}
-        <div className="contrato-section">
+        <div className="contrato-card mt-10 mx-auto">
           <h2 className="contrato-subtitle">
             Enviar Contrato
           </h2>
@@ -66,22 +62,25 @@ const ContractsAdmin = () => {
           <div className="contrato-actions">
             <button 
               onClick={handleCargarContrato} 
-              className="btn-gradient"
+              className="btn-primary-custom"
             >
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="M440-320v-326L336-542l-56-58 200-200 200 200-56 58-104-104v326h-80ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
               Cargar Contrato
             </button>
             
             <button 
               onClick={handleEnviarContrato} 
-              className="btn-gradient"
+              className="btn-primary-custom"
             >
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="M120-160v-640l760 320-760 320Zm66.67-102 520.66-218-520.66-220v158.67L428-480l-241.33 60v158Zm0 0v-438 438Z"/></svg>
               Enviar Contrato
             </button>
             
             <button 
               onClick={handleEliminarContrato} 
-              className="btn-gradient"
+              className="btn-primary-custom"
             >
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
               Eliminar contrato
             </button>
           </div>
@@ -90,7 +89,7 @@ const ContractsAdmin = () => {
         {/* Botón externo */}
         <button 
           onClick={handleVerListado}
-          className="btn-pink"
+          className="btn-secondary-custom mx-auto "
         >
           Ver Listado De Contratos
         </button>

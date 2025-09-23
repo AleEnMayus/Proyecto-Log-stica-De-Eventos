cf330744ebcd208c6154a7ff88a721fa9c4b8d5e
AleEnMayus <calejandrito317@gmail.com>
2025-09-23 18:54:15 -0500

    db de routes

--- BackEnd/routes/Admin/contractRoutes.js
+++ BackEnd/routes/Admin/contractRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const pool = require("../db");
+const pool = require("../../db");
 
 const router = express.Router();
 

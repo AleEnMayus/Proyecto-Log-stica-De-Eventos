c2fbcfadb1436ce8355331d777790b673ceb360c
Nury Navarrete-05 <nurygabrielanavarrete@gmail.com>
2025-10-29 22:11:25 -0500

    correción

--- FrontEnd/src/Views/admin/Resource/HomeResource.jsx
+++ FrontEnd/src/Views/admin/Resource/HomeResource.jsx
@@ -187,24 +187,6 @@ const ListResource = () => {
                   <td>{r.StatusDescription}</td>
                   <td>{r.Price}</td>
                   <td>
-<<<<<<< HEAD
-                    <div className="d-flex justify-content-center gap-2">
-                      <button
-                        className="btn-custom btn-edit-custom"
-                        onClick={() => handleEdit(r.ResourceId)}
-                        aria-label="Editar"
-                      >
-                        ✏️
-                      </button>
-                      <button
-                        className="btn-custom btn-delete-custom"
-                        onClick={() => handleDelete(r.ResourceId)}
-                        aria-label="Eliminar"
-                      >
-                        🗑️
-                      </button>
-                    </div>
-=======
                     <button
                       className="btn-custom btn-edit-custom mx-auto"
                       onClick={() => handleEdit(r.ResourceId)}
@@ -225,7 +207,6 @@ const ListResource = () => {
                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                       </svg>
                     </button>
->>>>>>> f7b498716cbb3cd62323a4653510b64a6e6c1cb8
                   </td>
                 </tr>
               ))}

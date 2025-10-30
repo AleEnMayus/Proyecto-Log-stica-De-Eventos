f7b498716cbb3cd62323a4653510b64a6e6c1cb8
AleEnMayus <calejandrito317@gmail.com>
2025-10-29 20:28:11 -0500

    eliminacion archivos innecesarios

--- BackEnd/routes/Admin/Calendar.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchAllEvents } = require('../controllers/eventController');
-
-router.get('/', fetchAllEvents);
-
-module.exports = router;
